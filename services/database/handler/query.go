cb4fecbcc9c8fb529181aeb95c1f3e54935630ba
Alex Zhang <hijifiji@hotmail.com>
2024-06-16 04:47:16 -0400

    query done

--- services/database/handler/query.go
+++ services/database/handler/query.go
@@ -2,12 +2,42 @@ package handler
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
 	"net/http"
 )
+
+type QueryRequest struct {
+	Query     string `json:"query"`
+	Arguments []any  `json:"arguments,omitempty"`
+}
 
 type Query struct {
 	Database *sql.DB
 }
 
 func (q *Query) AnyQuery(w http.ResponseWriter, r *http.Request) {
+	parsedRequest, err := parseRequestJSON(r)
+	if err != nil {
+		fmt.Println("error decoding incoming request: %w", err)
+		return
+	}
+
+	_, err = q.Database.Exec(parsedRequest.Query, parsedRequest.Arguments...)
+	if err != nil {
+		fmt.Println("error executing statement: ", err)
+	}
+}
+
+// Parser, consider moving to utils area in the future
+func parseRequestJSON(r *http.Request) (*QueryRequest, error) {
+	decoder := json.NewDecoder(r.Body)
+
+	ret := &QueryRequest{}
+	err := decoder.Decode(ret)
+	if err != nil {
+		return nil, err
+	}
+
+	return ret, nil
 }
