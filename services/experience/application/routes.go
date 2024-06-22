567c59ca5effc9fc7a7afd17fd7c56269c0f54b4
Alex Zhang <hijifiji@hotmail.com>
2024-06-21 23:24:17 -0400

    experience service

---
+++ services/experience/application/routes.go
@@ -0,0 +1,24 @@
+package application
+
+import (
+	"Alexjhz07/GoBot/services/experience/handler"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func (a *App) loadRoutes() {
+	router := chi.NewRouter()
+
+	router.Use(middleware.Logger)
+
+	router.Route("/experience", a.loadExperienceRoutes)
+
+	a.router = router
+}
+
+func (a *App) loadExperienceRoutes(router chi.Router) {
+	experienceHandler := &handler.Experience{}
+
+	router.Patch("/", experienceHandler.ModifyExperience)
+}
