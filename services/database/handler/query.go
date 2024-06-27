93d58145ec8bb7dd6cfc7390d4028c8dd5837013
Alex Zhang <hijifiji@hotmail.com>
2024-06-27 00:44:45 -0400

    query get stub

---
+++ services/database/handler/query.go
@@ -0,0 +1,36 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (db *Database) SimpleQuery(w http.ResponseWriter, r *http.Request) {
+	parsedRequest, err := parseRequestJSON(r)
+	if err != nil {
+		fmt.Println("error decoding incoming request: ", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	tx, err := db.Database.Begin()
+	if err != nil {
+		fmt.Println("error starting transaction")
+		return
+	}
+
+	defer tx.Rollback()
+
+	for i := 0; i < len(parsedRequest.Query); i++ {
+		rows, err := tx.Query(parsedRequest.Query[i], parsedRequest.Arguments[i]...)
+		if err != nil {
+			fmt.Println("error executing statement: ", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		fmt.Println(rows)
+	}
+
+	tx.Commit()
+	w.WriteHeader(http.StatusOK)
+}
