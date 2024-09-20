642241bdbebfeb2242141d1f7bbf6c2b55acd69c
Alex Zhang <hijifiji@hotmail.com>
2024-09-20 19:40:45 -0400

    fix: recreation funds display

--- bot/cogs/recreation.py
+++ bot/cogs/recreation.py
@@ -10,14 +10,14 @@ class Recreation(Cog):
     async def bet(self, ctx: Context, amount: str, prediction: str):
         try:
             prediction = prediction[0].lower()
-            amount = int(amount)
+            amount = int(amount) * 100
         except Exception as e:
             return await ctx.reply('Error: Amount is invalid')
         
         if amount <= 0: return await ctx.reply('Error: Amount must be greater than 0')
         if prediction not in ['h', 't']: return await ctx.reply('Error: Valid bets are `h`, `t`, or any word that starts with those letters')
         bal = await fetch_balance(ctx.author.id)
-        if bal < amount: await ctx.reply(f'Error: Insufficent balance: {bal}')
+        if bal < amount: await ctx.reply(f'Error: Insufficent balance: {bal / 100}')
 
         # I know that this could just be a 50/50 random implementation.
         # However, some things feel different even if they are the same
@@ -26,10 +26,10 @@ class Recreation(Cog):
         prediction = 'heads' if prediction == 'h' else 'tails'
         if prediction == result:
             await add_funds(ctx.author.id, amount, 'flip')
-            await ctx.reply(f'Success! You bet `{prediction}` and the coin landed `{result}` up.\nYou gained {amount} peanuts, your pockets now contain {bal + amount} peanuts.')
+            await ctx.reply(f'Success! You bet `{prediction}` and the coin landed `{result}` up.\nYou gained {amount / 100} peanuts, your pockets now contain {(bal + amount) / 100} peanuts.')
         else:
             await add_funds(ctx.author.id, -amount, 'flip')
-            await ctx.reply(f'Bad luck... You bet `{prediction}` and the coin landed `{result}` up.\nYou lost {amount} peanuts, your pockets now contain {bal - amount} peanuts.')
+            await ctx.reply(f'Bad luck... You bet `{prediction}` and the coin landed `{result}` up.\nYou lost {amount / 100} peanuts, your pockets now contain {(bal - amount) / 100} peanuts.')
 
     @commands.command(brief='A pleasant game of wordle', aliases=['guess', 'w'], ignore_extra=False)
     @commands.cooldown(1, 1, BucketType.user)
